6899dc7b73c0d2f3296bf3cceb05862019014ea3
bipasana1 <bipasana12@gmail.com>
2024-04-16 16:58:40 -0400

    database

--- app/core/init.php
+++ app/core/init.php
@@ -8,6 +8,12 @@ define('APP_VIEWS', APP_PUBLIC . '/assets/views');
 define('APP_STYLES', APP_PUBLIC . '/assets/styles');
 define('APP_IMAGES', APP_PUBLIC . '/assets/views/images');
 
+// Database configuration constants
+define('DB_NAME', $_ENV['DBNAME']);
+define('DB_HOST', $_ENV['DBHOST']);
+define('DB_USER', $_ENV['DBUSER']);
+define('DB_PASS', $_ENV['DBPASS']);
+
 spl_autoload_register(function ($classname) {
     require $filename = BASE_PATH . '/' . str_replace('\\', '/', $classname) . ".php";
 });
