80eada9d2de3f155b565988146719a7cd728e144
bipasana1 <bipasana12@gmail.com>
2024-04-17 08:34:33 -0400

    updates

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,7 +9,7 @@ class MainController extends Controller
 
     public function homepage()
     {
-                include '../public/assets/views/main/homepage.html';
+                include '../public/assets/views/main/homepage.php';
 //        $this->view('../public/assets/views/main/example-homepage.php', true);
     }
     public function resume()
