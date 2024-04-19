bfc30f41a2447359b5860515661e57c02dab9ae2
bipasana1 <bipasana12@gmail.com>
2024-04-19 09:03:04 -0400

    contact

--- public/assets/views/main/contact.php
+++ public/assets/views/main/contact.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Bipasana Poudel's Portfolio</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/styles/style.css"></head>
 </head>
@@ -28,8 +29,7 @@
     <div class="contact">    
         <h2>Contact Me</h2><br>
         <p>Hello! My name is Bipasana Poudel. I am studying computer science at Fordham University. I would love to hear from you!</p>
-        <form>
-    <form id="form-id">
+    <form id="form-id" method="POST">
         <input for="InputFullname" type="text" id="fullname" name="fullname" placeholder="Full Name"><br>
         <input for="InputEmail" type="text" id="email" name="email" placeholder="Email"><br>
         <input for="InputPhonenum" type="text" id="phonenum" name="phonenum" placeholder="Phone Number"><br>
@@ -62,7 +62,7 @@
           message,
         };
         $.ajax({
-          url: `http://localhost:8888/contact#success`,
+          url: `http://localhost:8888/contact`,
           type: "POST",
           data: data,
           dataType: "json",
